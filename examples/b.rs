733406d76cd894ad17df1dc3651d4618ec4efc23
José Armando García Sancio <jsancio@gmail.com>
2018-11-23 15:22:08 -0800

    Add crate features

--- examples/b.rs
+++ examples/b.rs
@@ -1,1 +1,5 @@
+#![feature(async_await, futures_api)]
+
 pub fn b() {}
+
+pub async fn b_async() {}
--- examples/c.rs
+++ examples/c.rs
@@ -1,7 +1,17 @@
-use b::b;
-use example_crate::a;
+#![feature(async_await, futures_api, await_macro)]
+
+mod b;
+
+use {
+    self::b::{b, b_async},
+    example_crate::a,
+};
 
 fn c() {}
+
+pub async fn c_async() {
+    await!(b_async());
+}
 
 fn main() {
     a();
